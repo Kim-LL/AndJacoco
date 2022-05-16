8632f5d1a0e4550ba3bfd1c9b3b2226f992678cd
Joyme <jinlianlong@joyme.sg>
2022-05-16 22:28:14 +0800

    更新代码

--- app/shell/pullDiffClass.sh
+++ app/shell/pullDiffClass.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-oriBran=$(git name-rev --name-only HEAD) # 获取当前远程分支名字
+oriBran=$(git name-rev --name-only HEAD)
 echo "当前分支：$oriBran" #remotes/origin/main_DEALER-2932
 
 gitBran=$1 # 本地分支
@@ -10,13 +10,13 @@ echo "workDir=$workDir"
 echo "outDir=$outDir"
 
 echo "start checkout--"
-git checkout -b ${gitBran} origin/$gitBran
-git checkout -f ${itBran}
+git checkout -b $gitBran origin/$gitBran
+git checkout -f $gitBran
 
 echo "start pull--"
 git pull
 
-echo "start copy: cp -r "${workDir}/app/classes" ${outDir} "
-cp -r "${workDir}/app/classes" ${outDir}
+echo "start copy: cp -r "${workDir}/app/classes" $outDir "
+cp -r "${workDir}/app/classes" $outDir
 
 echo "copy over --"
