f5bdcbcdcba24b1b2f2893095de593fbce188f45
Joyme <jinlianlong@joyme.sg>
2022-05-17 11:02:39 +0800

    save

--- rt-no-op/src/main/java/org/jacoco/agent/rt/CodeCoverageManager.java
+++ rt-no-op/src/main/java/org/jacoco/agent/rt/CodeCoverageManager.java
@@ -8,10 +8,10 @@ import android.content.Context;
  * Date: 2020/9/16
  * Description: 空代码实现，用于正式编译
  */
-public class ReleaseCodeCoverageManager {
+public class CodeCoverageManager {
 
 
-    private ReleaseCodeCoverageManager() {
+    private CodeCoverageManager() {
     }
 
 
