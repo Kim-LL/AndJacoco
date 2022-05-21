98249d96d4c059d1bc3a938ed93d0b5e2aeb671e
Joyme <jinlianlong@joyme.sg>
2022-05-21 16:03:47 +0800

    更新main方法

--- app/src/main/java/com/andjacoco/demo/activity/MainActivity.kt
+++ app/src/main/java/com/andjacoco/demo/activity/MainActivity.kt
@@ -7,11 +7,14 @@ import com.andjacoco.demo.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        BaseAction.Toast(this, "hello132")
+//        BaseAction.Toast(this, "hello132")
+//        BaseAction.Toast(this, "hello132")
+        BaseAction.Toast(this, "add")
 
         tv.setOnClickListener {
             startActivity(Intent(this, SecondActivity::class.java))
